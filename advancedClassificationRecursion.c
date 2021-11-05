3d3abb2a121951bcf6e10a15f26e53f69f49b8fe
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2021-11-05 14:24:59 +0200

    udating function isArmStrong

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,36 +1,55 @@
 #include<stdio.h>
 #include<math.h>
-#define Factorial()
-#define isArmStrong()
-#define isArmStrongHelper()
-#define isPalindrome()
-#define isPalindromeHelper()
 
-int Factorial(int n){
-    if(n <= 0)
-    return 1;
-    return Factorial(n-1)*n;
+int power(int n,int p){
+    int ans=1,i;
+    for(i=1;i<=p;i++){
+        ans = ans*n;
+    }
+    return ans;
 }
 
-int isArmStrongHelper(int n){
-    int sum=0, mult;
-    if(n > 0){
-        mult = 1;
-        int r = n % 10;
-        isArmStrong(n/10);
-        mult = Factorial(r);
-        sum += mult;
+int isArmStrongHelper(int n,int digit,int sum){
+    int r;
+    if(n==0){
+        return sum;
     }
-    return sum;
+    if(n>0){
+        r = n%10;
+        sum = sum + power(r,digit);
+        return isArmStrongHelper(n/10,digit,sum);
+    }
+    return 0;
+}
+
+int coutDigit(int n){
+    int temp = n;
+    int digit = 0;
+    while(temp>0){
+        digit++;
+        temp=temp/10;
+    }
+    return digit;
 }
 
 int isArmStrong(int n){
-    if(n == isArmStrongHelper(n)){
+    int digit = coutDigit(n);
+    if(isArmStrongHelper(n,digit,0) == n){
         return 1;
     }
     else{
         return 0;
     }
+}
+
+
+
+int isPalindromeHelper(int n, int temp){
+    if(n == 0){
+        return temp;
+    }
+    temp = (temp*10) + (n%10);
+    return isPalindromeHelper(n/10, temp);
 }
 
 int isPalindrome(int n){
@@ -41,12 +60,3 @@ int isPalindrome(int n){
         return 0;
     }
 }
-
-int isPalindromeHelper(int n, int temp){
-    if(n == 0){
-        return temp;
-    }
-    temp = (temp*10) + (n%10);
-    return isPalindromeHelper(n/10, temp);
-}
-
