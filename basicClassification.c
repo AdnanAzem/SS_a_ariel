96fdd3f4dcbe81eb9a39484918c8512761ebc410
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2021-11-05 14:27:45 +0200

    upaditing basic class

--- basicClassification.c
+++ basicClassification.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <math.h>
-#define isPrime()
-#define isStrong()
 
 int isPrime(int n){
-    for(i=2; i< num; i++){
-        if(num%i == 0)
+    int i;
+    for(i=2; i< n; i++){
+        if(n%i == 0)
         return 0;
     }
     return 1;
